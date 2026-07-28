a4b765d2676c9ce2c3e0d5d27329438fc7cd7f55
Justin <justinjones38@myyahoo.com>
2026-07-28 08:55:18 -0500

    improved navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
-import styles from "./Navbar.module.css"
+import styles from "./Navbar.module.css";
 import { IoLogoGameControllerA } from "react-icons/io";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
 import { Link, NavLink } from "react-router";
 import { useState } from "react";
-
 
 export default function Navbar() {
   const [isMenuShown, setIsMenuShown] = useState(false);
@@ -12,36 +11,60 @@ export default function Navbar() {
     <nav>
       <div className={styles.contentWrapper}>
         <Link to="." className={styles.leftNav}>
-          <IoLogoGameControllerA className={styles.logo} />
+          <IoLogoGameControllerA className={styles.icon} />
           <h1 className={styles.title}>byte&bits</h1>
         </Link>
         <div className={styles.rightNav}>
-          <GiHamburgerMenu 
-            className={styles.logo} 
+          <GiHamburgerMenu
+            className={`${styles.icon} ${styles.menu}`}
             onClick={() => setIsMenuShown(true)}
           />
-          <div className={isMenuShown ? `${styles["navMenu"]} ${styles["show"]}` : `${styles["navMenu"]}`}>
+          <div
+            className={
+              isMenuShown
+                ? `${styles["navMenu"]} ${styles["show"]}`
+                : `${styles["navMenu"]}`
+            }
+          >
             <IoMdClose
-              className={`${styles.logo} ${styles.closeBtn}`}
+              className={`${styles.icon} ${styles.closeBtn}`}
               onClick={() => setIsMenuShown(false)}
             />
             <ul className={styles.navList}>
               <li className={styles.navItem}>
-                <NavLink to="#"> My Posts</NavLink>
+                <NavLink 
+                  to="#" 
+                  className={styles.navLink}
+                  onClick={() => setIsMenuShown(false)}
+                  >
+                    
+                  My Posts
+                </NavLink>
               </li>
               <li className={styles.navItem}>
-                <Link to="#">Crew New Post</Link>
+                <Link 
+                  to="#" 
+                  className={styles.navLink}
+                  onClick={() => setIsMenuShown(false)}
+                  >
+                    
+                  Create New Post
+                </Link>
               </li>
               <li className={styles.navItem}>
-                <NavLink to="#"> View Settings</NavLink>
+                <NavLink 
+                  to="#" 
+                  className={styles.navLink}
+                  onClick={() => setIsMenuShown(false)}
+                  >
+                    
+                  View Settings
+                </NavLink>
               </li>
             </ul>
           </div>
-
         </div>
       </div>
-
-
     </nav>
-  )
+  );
 }

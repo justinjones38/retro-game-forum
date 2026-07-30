e81e51adce25e3c19fa9c02053bee1b021b80522
Justin <justinjones38@myyahoo.com>
2026-07-30 12:36:36 -0500

    added loading screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,19 @@ import styles from "./Home.module.css"
 import { useEffect, useState } from "react";
 import { supabase } from "../services/createClient";
 import Post from "../components/Post";
+import Loading from "../components/Loading";
 
 export default function Home() {
   const username = localStorage.getItem("username");
   const {isLoggedIn} = useOutletContext();
   const [posts, setPosts] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async() => {
       try {
+        setLoading(true)
         const {data, error} = await supabase
           .from("posts")
           .select()
@@ -23,6 +27,8 @@ export default function Home() {
         setPosts(data)
       } catch(error) {
         console.log("error, cannot fetch data")
+      } finally {
+        setLoading(false);
       }
 
     }
@@ -31,18 +37,19 @@ export default function Home() {
 
   console.log(posts);
   
-  if(!posts) {
-    return;
-  }
+
 
   return (
     <section className={styles.container}>
       {isLoggedIn ? <h2 className={styles.title}>Hello {username}! Here are some great recent posts</h2> : null}
+      {loading ? <Loading /> : null}
+  
+      {!loading && !error && posts ? 
       <ul className={styles.postList}>
         {posts.map(post => (
           <Post key={post.id} post={post} />
         ))}
-      </ul>
+      </ul> : null}
     </section>
   )
 }

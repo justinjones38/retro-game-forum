3a708466079b62493ef2a3e24eb976de4c1823a6
Justin <justinjones38@myyahoo.com>
2026-08-02 23:30:11 -0500

    improved functionality of navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,12 +10,11 @@ import useWindowWidth from "../../hooks/useWindowWidth";
 export default function Navbar({ isLoggedIn, setIsLoggedIn }) {
   const [isMenuShown, setIsMenuShown] = useState(false);
   const { windowWidth } = useWindowWidth();
-  const [username, setUsername] = useState(
-    (localStorage.getItem("username")),
-  );
+  const [username, setUsername] = useState(localStorage.getItem("username"));
   console.log(Boolean(username));
 
   const handleLogOut = () => {
+    console.log("run");
     setIsMenuShown(false);
     setIsLoggedIn(false);
     setUsername(null);
@@ -25,87 +24,81 @@ export default function Navbar({ isLoggedIn, setIsLoggedIn }) {
   return (
     <nav>
       <div className={styles.contentWrapper}>
-        <Link to="." className={styles.leftNav}>
-          <IoLogoGameControllerA className={styles.icon} />
-          <h1 className={styles.title}>byte&bits</h1>
-        </Link>
+        <div className={styles.leftNav}>
+          <Link to="." className={styles.leftNav}>
+            <IoLogoGameControllerA className={styles.icon} />
+            <h1 className={styles.title}>byte&bits</h1>
+          </Link>
+        </div>
         <div className={styles.rightNav}>
-          <GiHamburgerMenu
-            className={`${styles.icon} ${styles.hamburgerMenu}`}
-            onClick={() => setIsMenuShown(true)}
-          />
-          <div
-            className={
-              isMenuShown && windowWidth < 750
-                ? `${styles["navMenu"]} ${styles["show"]}`
-                : `${styles["navMenu"]}`
-            }
-          >
-            <IoMdClose
-              className={`${styles.icon} ${styles.closeBtn}`}
-              onClick={() => setIsMenuShown(false)}
-            />
-            <ul className={styles.navList}>
-              <li className={styles.navItem}>
-                <NavLink
-                  to="my-posts"
-                  className={({ isActive }) =>
-                    isActive
-                      ? `${styles.navLink} ${styles.active}`
-                      : `${styles.navLink}`
-                  }
+          {!isLoggedIn ? (
+            <Link to="login" className={styles.navBtn}>Log In</Link>
+          ) : (
+            <>
+              <GiHamburgerMenu
+                className={`${styles.icon} ${styles.hamburgerMenu}`}
+                onClick={() => setIsMenuShown(true)}
+              />
+              <div
+                className={
+                  isMenuShown && windowWidth < 750
+                    ? `${styles["navMenu"]} ${styles["show"]}`
+                    : `${styles["navMenu"]}`
+                }
+              >
+                <IoMdClose
+                  className={`${styles.icon} ${styles.closeBtn}`}
                   onClick={() => setIsMenuShown(false)}
-                >
-                  My Posts
-                </NavLink>
-              </li>
-              <li className={styles.navItem}>
-                <NavLink
-                  to="create-post"
-                  className={({ isActive }) =>
-                    isActive
-                      ? `${styles.navLink} ${styles.active}`
-                      : `${styles.navLink}`
-                  }
-                  onClick={() => setIsMenuShown(false)}
-                >
-                  Create New Post
-                </NavLink>
-              </li>
-              <li className={styles.navItem}>
-                <NavLink
-                  to="#"
-                  className={({ isActive }) =>
-                    isActive
-                      ? `${styles.navLink} ${styles.active}`
-                      : `${styles.navLink}`
-                  }
-                  onClick={() => setIsMenuShown(false)}
-                >
-                  My Account
-                </NavLink>
-              </li>
-              <li className={`${styles.navItem} ${styles.loginBtn}`}>
-                {!isLoggedIn ? (
-                  <NavLink
-                    to="login"
-                    className={({ isActive }) =>
-                      isActive
-                        ? `${styles.navLink} ${styles.active}`
-                        : `${styles.navLink}`
-                    }
-                    onClick={() => setIsMenuShown(false)}
-                  >
-                    Log in
-                  </NavLink>
-                ) : (
-                  <button onClick={handleLogOut} className={styles.navBtn}>
-                    Log Out
-                  </button>
-                )}
-              </li>
-            </ul>
-          </div>
+                />
+                <ul className={styles.navList}>
+                  <li className={styles.navItem}>
+                    <NavLink
+                      to="my-posts"
+                      className={({ isActive }) =>
+                        isActive
+                          ? `${styles.navLink} ${styles.active}`
+                          : `${styles.navLink}`
+                      }
+                      onClick={() => setIsMenuShown(false)}
+                    >
+                      My Posts
+                    </NavLink>
+                  </li>
+                  <li className={styles.navItem}>
+                    <NavLink
+                      to="create-post"
+                      className={({ isActive }) =>
+                        isActive
+                          ? `${styles.navLink} ${styles.active}`
+                          : `${styles.navLink}`
+                      }
+                      onClick={() => setIsMenuShown(false)}
+                    >
+                      Create New Post
+                    </NavLink>
+                  </li>
+                  <li className={styles.navItem}>
+                    <NavLink
+                      to="#"
+                      className={({ isActive }) =>
+                        isActive
+                          ? `${styles.navLink} ${styles.active}`
+                          : `${styles.navLink}`
+                      }
+                      onClick={() => setIsMenuShown(false)}
+                    >
+                      My Account
+                    </NavLink>
+                  </li>
+                  <li className={`${styles.navItem} ${styles.loginBtn}`}>
+                    <button onClick={handleLogOut} className={styles.navBtn}>
+                      Log Out
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </nav>

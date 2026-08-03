e4d9ca13ff42fb1688342fcb66e2978a743cc3a4
Justin <justinjones38@myyahoo.com>
2026-08-03 00:43:09 -0500

    added skeleton PostDetail page

--- src/pages/posts/PostDetail.jsx
+++ src/pages/posts/PostDetail.jsx
@@ -1,8 +1,33 @@
 import { useParams } from "react-router"
 import styles from "./PostDetail.module.css"
+import { useEffect } from "react";
+import { supabase } from "../../services/createClient";
 
 export default function PostDetail(){
   const {id} = useParams();
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const {data, error} = await supabase
+          .from(`posts`)
+          .select(`*, users(*)`)
+          .eq("id", id)
+          .single()
+        
+        console.log(data);
+        
+        
+        if(error) {
+          throw new Error("Cannot fetch data")
+        }
+      } catch(error) {
+        console.log(error.message);
+      }
+
+    }
+    fetchData();
+  }, [])
   console.log(id);
   return (
     <section className={styles.container}>

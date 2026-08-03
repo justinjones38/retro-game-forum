ead2bfaa17d232289eada0d0eda9aaa3e82fa9fd
Justin <justinjones38@myyahoo.com>
2026-08-03 00:18:35 -0500

    adjusted styling of Post Page

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,5 +1,5 @@
 import styles from "./Post.module.css"
-import { FaArrowCircleUp } from "react-icons/fa";
+import { FaArrowUp } from "react-icons/fa";
 import { getTimeDiff } from "../../utils/utils";
 import { Link } from "react-router";
 
@@ -9,13 +9,17 @@ export default function Post({post, handleLikePost}) {
     <li className={styles.postItem}>
       <div className={styles.leftItem}>
         <h3 className={styles.postTitle}>
-          <Link to="#">{post.title}</Link>
+          <Link to="#" className={styles.titleLink}>{post.title}</Link>
           </h3>
-        <p className={styles.time}>Uploaded {getTimeDiff(post.created_at)} ago</p>
-        <p className={styles.author}>Uploaded by {post.users.username}</p>
+        <p 
+          className={styles.description}
+          aria-label={`uploaded by ${post.users.username}, posted ${getTimeDiff(post.created_at)} ago`}
+        >
+          {post.users.username} - <span>{getTimeDiff(post.created_at)}</span> ago
+        </p>
       </div>
       <div className={styles.rightItem}>
-        <button className={styles.btn} onClick={() => handleLikePost(post.id)}><FaArrowCircleUp className={styles.icon} /></button>
+        <button className={styles.btn} onClick={() => handleLikePost(post.id)}><FaArrowUp className={styles.icon} /></button>
         <p 
           className={styles.likesCount}
           aria-label={`${post.likes} ${post.likes ===  1 ? "like" : "likes"}`}

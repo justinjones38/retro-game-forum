945fa253fe3bab061c1b1c1827ce50bcbed9752f
Justin <justinjones38@myyahoo.com>
2026-08-04 01:58:43 -0500

    improved styling of MyPost Page

--- src/components/posts/MyPost.jsx
+++ src/components/posts/MyPost.jsx
@@ -1,5 +1,5 @@
 import styles from "./MyPost.module.css"
-import { FaArrowCircleUp } from "react-icons/fa";
+import { FaArrowUp } from "react-icons/fa";
 import { getTimeDiff } from "../../utils/utils";
 import { Link } from "react-router";
 
@@ -8,13 +8,20 @@ export default function MyPost({post}) {
     <li className={styles.postItem}>
       <div className={styles.leftItem}>
         <h3 className={styles.postTitle}>
-          <Link to="#">{post.title}</Link>
+          <Link to={`/posts/${post.id}`} className={styles.postLink}>{post.title}</Link>
           </h3>
-        <p className={styles.time}>Uploaded {getTimeDiff(post.created_at)} ago</p>
+        <p className={styles.time}>posted {getTimeDiff(post.created_at)}</p>
       </div>
       <div className={styles.rightItem}>
-        <FaArrowCircleUp className={styles.icon} />
-        <p className={styles.likesCount}>153 upvotes</p>
+        <div className={styles.likesCounter}>
+          <FaArrowUp className={styles.icon} />
+          <p className={styles.likesCount}>{post.likes}</p>
+        </div>
+        <div className={styles.btnContainer}>
+          <button className={`${styles.btn} ${styles.editBtn}`}>Edit</button>
+          <button className={`${styles.btn} ${styles.deleteBtn}`}>Delete</button>
+        </div>
+
       </div>
     </li>
   )

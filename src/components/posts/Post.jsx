59c89f3ca17d505a9d530b738503c905e546026f
Justin <justinjones38@myyahoo.com>
2026-08-04 21:18:46 -0500

    added flags onto post page

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,31 +1,61 @@
-import styles from "./Post.module.css"
+import styles from "./Post.module.css";
 import { FaArrowUp } from "react-icons/fa";
 import { getTimeDiff } from "../../utils/utils";
 import { Link } from "react-router";
 
-export default function Post({post, handleLikePost}) {
+export default function Post({ post, handleLikePost }) {
   console.log(post);
+  {
+    post.flags ? (
+      <ul className={styles.flagList}>
+        {post.flags.map((flag, index) => (
+          <li key={index} className={styles.flagItem}>
+            {flag}
+          </li>
+        ))}
+      </ul>
+    ) : null;
+  }
   return (
     <li className={styles.postItem}>
-      <div className={styles.leftItem}>
-        <h3 className={styles.postTitle}>
-          <Link to={`posts/${post.id}`} className={styles.titleLink}>{post.title}</Link>
+      {post.flags ? (
+        <ul className={styles.flagList}>
+          {post.flags.map((flag, index) => (
+            <li key={index} className={styles.flagItem}>
+              {flag}
+            </li>
+          ))}
+        </ul>
+      ) : null}
+      <div className={styles.contentWrapper}>
+        <div className={styles.leftItem}>
+          <h3 className={styles.postTitle}>
+            <Link to={`posts/${post.id}`} className={styles.titleLink}>
+              {post.title}
+            </Link>
           </h3>
-        <p 
-          className={styles.description}
-          aria-label={`uploaded by ${post.users.username}, posted ${getTimeDiff(post.created_at)} ago`}
-        >
-          <Link to={`/users/${post.users.username}`}>{post.users.username}</Link> - <span>{getTimeDiff(post.created_at)}</span>
-        </p>
-      </div>
-      <div className={styles.rightItem}>
-        <button className={styles.btn} onClick={() => handleLikePost(post.id)}><FaArrowUp className={styles.icon} /></button>
-        <p 
-          className={styles.likesCount}
-          aria-label={`${post.likes} ${post.likes ===  1 ? "like" : "likes"}`}
-        >{post.likes}
-        </p>
+          <p
+            className={styles.description}
+            aria-label={`uploaded by ${post.users.username}, posted ${getTimeDiff(post.created_at)} ago`}
+          >
+            <Link to={`/users/${post.users.username}`}>
+              {post.users.username}
+            </Link>{" "}
+            - <span>{getTimeDiff(post.created_at)}</span>
+          </p>
+        </div>
+        <div className={styles.rightItem}>
+          <button className={styles.btn} onClick={() => handleLikePost(post.id)}>
+            <FaArrowUp className={styles.icon} />
+          </button>
+          <p
+            className={styles.likesCount}
+            aria-label={`${post.likes} ${post.likes === 1 ? "like" : "likes"}`}
+          >
+            {post.likes}
+          </p>
+        </div>
       </div>
     </li>
-  )
+  );
 }
